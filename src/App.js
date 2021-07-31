b24227f1117a19a4b8961bf7d5dc25576373c108
Mosharraf-Musa <mosharrafny@gmail.com>
2021-07-31 19:11:51 -0400

    useState-hook

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css';
+import LessText from './components/LessText';
 
 function App() {
-  const [count, setCount] = useState(0);
-  function increment() {
-    setCount(prevCount => prevCount + 1)
-  }
-  function decrement() {
-    setCount(prevCount => prevCount - 1)
-  }
+
   return (
     <div className="App">
-      <button onClick={increment}>+</button>
-      <span>{count}</span>
-      <button onClick={decrement}>-</button>
+      <LessText text={`Focused, hard work is the real key
+      to success. Keep your eyes on the goal, 
+      and just keep taking the next step 
+      towards completing it.`} maxLength={35} />
     </div>
   );
 }
