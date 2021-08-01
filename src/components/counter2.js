92b819945e890038f32f955a4b6dbdb7af4070b7
Mosharraf-Musa <mosharrafny@gmail.com>
2021-08-01 18:26:21 -0400

    learn usestate

---
+++ src/components/counter2.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+const LessText = ({ text, maxLength }) => {
+    const [hidden, setHidden] = useState(true);
+    if (text.length <= maxLength) {
+        return <span>{text}</span>
+    }
+    return (
+        <span>
+            {hidden ? `${text.substr(0, maxLength).trim()} ...` : text}
+            {hidden ? (
+                <a onClick={() => setHidden(false)}> read more</a>
+            ) : (
+                <a onClick={() => setHidden(true)}> read less</a>
+            )}
+
+        </span>
+    )
+}
+
+export default LessText
